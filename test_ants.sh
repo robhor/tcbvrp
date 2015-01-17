a4dadf902f097c95dafaf615f5810c6d3b99c659
Johannes Vogel <johannes.vogel@hotmail.com>
2015-01-17 23:39:54 +0100

    update testfile

--- test_ants.sh
+++ test_ants.sh
@@ -13,32 +13,47 @@ for p in 10 30 60 90 120 180 300 400 500; do
 	i=1
 	
 		stime=$(date +"%T")
-		echo "Run $i started at $stime"
+		echo "Run $i with pheromone = 1 started at $stime"
 		before=$(date +%s)
-		./tcbvrp $f > Results/ants_3/$p/try$i.out 2> Results/ants_3/$p/try$i.err
+		./tcbvrp --ant1 $f > Results/test_ants/$p/try$i.out 2> Results/test_ants/$p/try$i.err
 		after=$(date +%s)
-		echo "Instance $f.. elapsed time:" $((after - $before)) "seconds"	> Results/ants_3/times/time_$p/try_$i.time
-		echo "Saved: Instance $f.. elapsed time:" $((after - $before)) "seconds"
-		#let i=$i+1	
+		echo "Instance $f with pheromone = 1 .. elapsed time:" $((after - $before)) "seconds"	> Results/test_ants/times/time_$p/try_$i_ph_1.time
+		echo "Saved: Instance $f with pheromone = 1 .. elapsed time:" $((after - $before)) "seconds"
+		
+		stime=$(date +"%T")
+		echo "Run $i with pheromone = 2 started at $stime"
+		before=$(date +%s)
+		./tcbvrp --ant2 $f > Results/test_ants/$p/try$i.out 2> Results/test_ants/$p/try$i.err
+		after=$(date +%s)
+		echo "Instance $f with pheromone = 2 .. elapsed time:" $((after - $before)) "seconds"	> Results/test_ants/times/time_$p/try_$i_ph_2.time
+		echo "Saved: Instance $f with pheromoe = 2 .. elapsed time:" $((after - $before)) "seconds"
+
+	
 done
 
 for p in 10 30 60 90 120 180 300 400 500; do
-	
 	f=Instances/tcbvrp_$p.prob
 	mkdir -p Results/test_ants/$p
 	mkdir -p Results/test_ants/times/time_$p
 	echo "Processing ants $f..";
 	i=1
-	while [ $i -le 29 ]
+	while [ $i -le 14 ]
 	do
 		stime=$(date +"%T")
-		echo "Run $i started at $stime"
+		echo "Run $i with pheromone = 1 started at $stime"
 		before=$(date +%s)
-		./tcbvrp $f > Results/test_ants/$p/try$i.out 2> Results/test_ants/$p/try$i.err
+		./tcbvrp --ant1 $f > Results/test_ants/$p/try$i.out 2> Results/test_ants/$p/try$i.err
 		after=$(date +%s)
-		echo "Instance $f.. elapsed time:" $((after - $before)) "seconds"	> Results/test_ants/times/time_$p/try_$i.time
-		echo "Saved: Instance $f.. elapsed time:" $((after - $before)) "seconds"
-		let i=$i+1	
+		echo "Instance $f with pheromone = 1 .. elapsed time:" $((after - $before)) "seconds"	> Results/test_ants/times/time_$p/try_$i_ph_1.time
+		echo "Saved: Instance $f with pheromone = 1 .. elapsed time:" $((after - $before)) "seconds"
+		
+		stime=$(date +"%T")
+		echo "Run $i with pheromone = 2 started at $stime"
+		before=$(date +%s)
+		./tcbvrp --ant2 $f > Results/test_ants/$p/try$i.out 2> Results/test_ants/$p/try$i.err
+		after=$(date +%s)
+		echo "Instance $f with pheromone = 2 .. elapsed time:" $((after - $before)) "seconds"	> Results/test_ants/times/time_$p/try_$i_ph_2.time
+		echo "Saved: Instance $f with pheromoe = 2 .. elapsed time:" $((after - $before)) "seconds"
 	done
+	
 done
-
